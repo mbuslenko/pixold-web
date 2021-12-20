f49dd4cff5ce9a48044cba6869a8981585c64bba
MielomankA <mielomanka@mail.ru>
2021-12-20 05:52:56 +0300

    homepage: eject CRA, add 3 sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,76 @@
-import './App.scss';
+import './App.less';
+import Button from './components/button/Button';
+import { ButtonStyles } from './enums';
 
 function App() {
   return (
     <>
-      <div className="App">Pixold-web</div>
+      <header className="header">
+        <div className="container">
+          <div className="header-content"></div>
+        </div>
+      </header>
+      <div className="start-wrap">
+        <section className="start">
+          <div className="logo"></div>
+          <h1 className="title">There will be a slogan</h1>
+          <div className="wrap-btn">
+            <Button textContent="Get started" styles={ButtonStyles.primary} action={() => {}} width={132} height={44} />
+          </div>
+        </section>
+      </div>
+      <section className="info">
+        <div className="info-content">
+          <div className="info-bg"></div>
+          <div className="info-description">
+            <h2 className="info-title">There will be a header</h2>
+            <p className="info-desc">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit ultricies aliquam quis in accumsan, vel
+              ut. Posuere suscipit neque scelerisque libero. Quisque ipsum tristique arcu velit facilisi nec lectus.
+              Commodo sed elementum congue consequat.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="polygons">
+        <div className="container">
+          <div className="polygons-content">
+            <h2 className="info-title polygon-title">There will be a header</h2>
+            <div className="polygon-attack">
+              <div className="polygon-attack-bg"></div>
+              <div className="polygon-title-card">Attack</div>
+              <div className="polygon-btn">
+                <Button textContent="Read more" styles={ButtonStyles.red} action={() => {}} width={127} height={44} />
+              </div>
+            </div>
+            <div className="polygon-miner">
+              <div className="polygon-miner-bg"></div>
+              <div className="polygon-title-card">Miner</div>
+              <div className="polygon-btn">
+                <Button
+                  textContent="Read more"
+                  styles={ButtonStyles.yellow}
+                  action={() => {}}
+                  width={127}
+                  height={44}
+                />
+              </div>
+            </div>
+            <div className="polygon-defender">
+              <div className="polygon-defender-bg"></div>
+              <div className="polygon-title-card">Defender</div>
+              <div className="polygon-btn">
+                <Button textContent="Read more" styles={ButtonStyles.blue} action={() => {}} width={127} height={44} />
+              </div>
+            </div>
+            <div className="polygon-desc">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit ultricies aliquam quis in accumsan, vel
+              ut. Posuere suscipit neque scelerisque libero. Quisque ipsum tristique arcu velit facilisi nec lectus.
+              Commodo sed elementum congue consequat.
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
