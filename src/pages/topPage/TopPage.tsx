0445a3e270ae3c320d6f7c3571cdfddf3128311f
MielomankA <mielomanka@mail.ru>
2021-12-30 02:52:42 +0300

    top-page: add modal, component TopPlayer

--- src/pages/topPage/TopPage.tsx
+++ src/pages/topPage/TopPage.tsx
@@ -1,56 +1,79 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { blockScrolling, unblockScrolling } from '../../shared/ts/helperFunctions';
+
 import { Button } from '../../components/ui-kit/button/Button';
+import { Modal } from '../../components/ui-kit/modal/Modal';
 
 import './TopPage.scss';
+import { TopPlayer } from './TopPlayer';
 
-/* interface ICellContent {
-  content: string | number;
-}
-
-export const Cell: React.FC<ICellContent> = ({ content }) => {
-  return <div>{content}</div>;
-}; */
+const arrTopPlayers = [
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+  { username: '', place: '', score: '' },
+];
 
 export const TopPage: React.FC = () => {
+  const [isVisibleModalPreview, setIsVisibleModalPreview] = useState<boolean>(false);
+
+  const showModalPreview = (): void => {
+    blockScrolling();
+    setIsVisibleModalPreview(true);
+  };
+
+  const hideModalPreview = (): void => {
+    setIsVisibleModalPreview(false);
+    unblockScrolling();
+  };
+
+  const modalHeading = 'All seats are already taken';
+
+  const modalText =
+    "We are very sorry, but we have already signed up enough people to test. We try to release it as soon as possible, so you won't have to wait long!";
+
   return (
-    <div className="top-wrap">
-      <div className="top-content">
-        <div className="top-back-wrap">
-          <Button text="← Back to game" priority="secondary" className="top-back" />
-        </div>
-        <div className="top-info">
-          <h1 className="top-title">Top Players page</h1>
-          <div className="top-desc">Will be soon</div>
-          <div className="top-table">
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-1"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-2"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
-            <div className="top-cell cell-3"></div>
+    <>
+      <div className={`top-wrap ${isVisibleModalPreview && 'is-blurred'}`}>
+        <div className="top-content">
+          <div className="top-back-wrap">
+            <Link to="/game">
+              <Button text="← Back to game" priority="secondary" className="top-back" />
+            </Link>
+          </div>
+          <div className="top-info">
+            <h1 className="top-title">Top Players page</h1>
+            <div className="top-desc">Will be soon</div>
+            <div className="top-table">
+              {arrTopPlayers.map((_, index) => {
+                return (
+                  <TopPlayer
+                    username={arrTopPlayers[index].username}
+                    place={arrTopPlayers[index].place}
+                    score={arrTopPlayers[index].score}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div className="top-show-wrap">
+            <Button text="Sign up for preview" priority="primary" className="top-show" onClick={showModalPreview} />
           </div>
         </div>
-        <div className="top-show-wrap">
-          <Button text="Sign up for preview" priority="primary" className="top-show" />
-        </div>
       </div>
-    </div>
+      {isVisibleModalPreview && (
+        <div className="top-modal-wrapper">
+          <Modal heading={modalHeading} text={modalText} sizeClassName="top-modal-size">
+            <Button text="Close" priority="primary" className="top-btn-close" onClick={hideModalPreview} />
+          </Modal>
+        </div>
+      )}
+    </>
   );
 };
