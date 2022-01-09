b25d5d001d5c9df127a88cf5c9628d46f615dd1d
MielomankA <mielomanka@mail.ru>
2022-01-09 21:31:53 +0300

    homepage-release: fix logo, routes

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { HomeWelcomeScreen } from './HomeWelcomeScreen';
 import { HomeInfoScreen } from './HomeInfoScreen';
 import { HomeHexagonsScreen } from './HomeHexagonsScreen';
-import { HomeOpenseaScreen } from './HomeOpenseaScreen';
 import { HomeGetStartedScreen } from './HomeGetStartedScreen';
 import { HomeFooter } from './HomeFooter';
 
@@ -13,7 +12,6 @@ export const HomePage: React.FC = () => {
       <HomeWelcomeScreen />
       <HomeInfoScreen />
       <HomeHexagonsScreen />
-      <HomeOpenseaScreen />
       <HomeGetStartedScreen />
       <HomeFooter />
     </>
