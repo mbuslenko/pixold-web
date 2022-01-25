2e22f11a855a9372c955728a69692008844fabf2
MielomankA <mielomanka@mail.ru>
2022-01-25 19:08:46 +0300

    logo-safari: replace img tag with object

--- src/pages/play/PlayMenu.tsx
+++ src/pages/play/PlayMenu.tsx
@@ -38,7 +38,8 @@ export const PlayMenu: React.FC = () => {
         <h2 className="play-menu-heading">Navigate</h2>
         <nav className="play-menu-navigation">
           <Link to="/home">
-            <img className="play-menu-logo" src={logo} alt="logo" />
+            {/* <img className="play-menu-logo" src={logo} alt="logo" /> */}
+            <object className="play-menu-logo" data={logo} type="image/svg+xml" />
           </Link>
           <div className="play-menu-link-container">
             <button className="play-menu-button" onClick={() => console.log(';)')}>
