febc1f61b95ae582b1580596da638dde4bd0aa2b
MielomankA <mielomanka@mail.ru>
2022-01-27 18:09:32 +0300

    number-animation: refactor

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -6,12 +6,10 @@ import { HomeHexagonsScreen } from './HomeHexagonsScreen';
 import { HomeOpenseaScreen } from './HomeOpenseaScreen';
 import { HomeGetStartedScreen } from './HomeGetStartedScreen';
 import { HomeFooter } from './HomeFooter';
-import { NumberAnimation } from '../../components/numberAnimation/NumberAnimation';
 
 export const HomePage: React.FC = () => {
   return (
     <>
-      <NumberAnimation number={1000000000} />
       <HomeWelcomeScreen />
       <HomeInfoScreen />
       <HomeHexagonsScreen />
