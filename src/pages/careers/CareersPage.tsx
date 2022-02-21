fd4e47772411aa018e9e23252b443e05843822bf
MielomankA <mielomanka@mail.ru>
2022-02-21 18:42:41 +0300

    careers: delete vacancy ui/ux

--- src/pages/careers/CareersPage.tsx
+++ src/pages/careers/CareersPage.tsx
@@ -21,26 +21,13 @@ export const CareersPage = () => {
         <div className="careers-page-open">
           <h2 className="careers-page-open-title">Open roles</h2>
           <p className="careers-page-open-desc">
-            We are looking for people like us with «burning eyes» to join our team. So if you would
-             like to work at Pixold but don't see a suitable position on the list, please contact us at hello@pixold.io
+            We are looking for people like us with «burning eyes» to join our team. So if you would like to work at
+            Pixold but don't see a suitable position on the list, please contact us at hello@pixold.io
           </p>
         </div>
         <a href="#ui-vacancy" className="careers-page-arrow-down">
           <img src={arrowDownSvg} alt="Arrow down" />
         </a>
-        <CareerVacancy
-          id="ui-vacancy"
-          className="careers-page-vacancy-ui-bg"
-          title="UI/UX Designer"
-          applyLink="hello@pixold.io"
-        >
-          <div>
-            <h3 className="careers-page-vacancy-title-small">Description</h3>
-            <p className="careers-page-vacancy-desc">
-              Web designer with good knowledge of Figma, knowledge of modern UI/UX approaches for the Web and sense of style, composition, form and color. Portfolio is required.
-            </p>
-          </div>
-        </CareerVacancy>
         <CareerVacancy className="careers-page-vacancy-back-bg" title="Backend Developer" applyLink="hello@pixold.io">
           <div>
             <h3 className="careers-page-vacancy-title-small">Language</h3>
